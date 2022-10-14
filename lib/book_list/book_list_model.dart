a25514f0a06e6b53cfbb38c6d5268660f3610f88
y129may9th <y129may9th@gmail.com>
2022-10-14 20:06:19 +0900

    chore: use firestore get()

--- lib/book_list/book_list_model.dart
+++ lib/book_list/book_list_model.dart
@@ -2,25 +2,25 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import '../domain/book.dart';
 
+// _xxxx はプライベート変数
 class BookListModel extends ChangeNotifier {
-  final Stream<QuerySnapshot> _usersStream =
-      FirebaseFirestore.instance.collection('books').snapshots();
-
   List<Book>? books;
 
-  void fetchBoookList() {
-    _usersStream.listen((QuerySnapshot snapshots) {
-      final List<Book> books = snapshots.docs.map((DocumentSnapshot document) {
-        Map<String, dynamic> data = document.data()! as Map<String, dynamic>;
-        print('passed');
-        print(data);
-        final String title = data['title'];
-        final String author = data['author'];
-        return Book(title, author);
-      }).toList();
+  void fetchBoookList() async {
+    final QuerySnapshot snapshot =
+        await FirebaseFirestore.instance.collection('books').get();
 
-      this.books = books;
-      notifyListeners();
-    });
+    final List<Book> books = snapshot.docs.map((DocumentSnapshot document) {
+      Map<String, dynamic> data = document.data()! as Map<String, dynamic>;
+      print('passed');
+      print(data);
+      final String title = data['title'];
+      final String author = data['author'];
+
+      return Book(title, author);
+    }).toList();
+
+    this.books = books;
+    notifyListeners();
   }
 }
